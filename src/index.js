6b70752d1b0088abccc3a5113748edccf72922fe
Kinskaya <kinskaya.nastya@gmail.com>
2021-02-18 23:10:11 +0300

    add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (num) {
+    let numNew = num.toString();
+    let result = '';
+    for (let i = 0; i < numNew.length; i++) {
+        if (numNew[i] !== '-') {
+            result = numNew[i] + result;
+        }
+    }
+    return +result;
 }
